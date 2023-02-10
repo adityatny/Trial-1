3c35d02290b0410b6eae70e55534d0cf753a1a89
aditya-kr007 <124397282+aditya-kr007@users.noreply.github.com>
2023-02-10 19:15:58 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    printf("Name:-Aditya Kumar\nReg No:-20223016\nDate:-02/02/22\n");
+    printf("Aim:-WAP to print the size of variables like int , float etc.\n\n\n");
+
+
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of float: %lubytes\n", sizeof(float));
+
+    return 0;
+}
